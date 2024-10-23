190d1c81a214441cb1dad629e120c17747c1b4b8
Cesar_Sierra <cesarmallungo@gmail.com>
2024-10-22 23:51:35 -0500

    Se mejora portafolio, ajusta archivo .cpanel.yml

--- src/components/MySlider.jsx
+++ src/components/MySlider.jsx
@@ -4,9 +4,13 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import TailwindIc from '../components/icons/react/Tailwind.jsx';
 import AstroIcon from '../components/icons/react/AstroIncon.jsx';
 import Django from '../components/icons/react/Django.jsx';
-import Android from '../components/icons/react/Android.jsx';
+import PythonIcon from '../components/icons/react/Python.jsx';
+import AndroidStudio from '../components/icons/react/Android.jsx';
+import Kali from '../components/icons/react/Kali.jsx';
+import { slide } from 'astro:transitions';
 
 
 
@@ -19,10 +23,14 @@ const MySlider = () => {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1500,
+        pauseOnHover: false,
+        arrows: true, 
+
+
     };
 
     return (
-
+        <div {...slide({ duration: 500 })}>
         <Slider {...settings}>
                 <div>
                     <i className="fab fa-html5 fa-3x" style={{ color: '#e34f26' }}></i>
@@ -33,34 +41,42 @@ const MySlider = () => {
                     <p>CSS3</p>
                 </div>
                 <div>
+                    <i><TailwindIc class="iconosJx" style={{ color: '#06B6D4' }}/> </i>
+                    <p>Tailwind CSS</p>
+                </div>
+                <div>
                     <i className="fab fa-js fa-3x" style={{ color: '#f7df1e' }}></i>
                     <p>JavaScript</p>
-                </div>
-                <div>
-                     <i><AstroIcon class="iconosJx"  /> </i>
-                    <p>Astro</p>
-                </div>
-                <div>
-                    <i className="fab fa-python fa-3x" style={{ color: '#306998' }}></i>
-                    <p>Python</p>
-                </div>
-                <div>
-                    <i><Django class="iconosJx" /> </i>
-                    <p>Django</p>
                 </div>
                 <div>
                     <i className="fab fa-node-js fa-3x" style={{ color: '#339933' }}></i>
                     <p>Node.js</p>
                 </div>
                 <div>
-                    <i className="fab fa-react fa-3x" style={{ color: '#61dafb' }}></i>
-                    <p>React</p>
+                     <i><AstroIcon class="iconosJx"  /> </i>
+                    <p>Astro</p>
                 </div>
                 <div>
-                    <i><Android class="iconosJx" /> </i>
-                    <p>Android</p>
+                    <i><PythonIcon class="iconosJx" /> </i>
+                    <p>Python</p>
                 </div>
+                <div>
+                    <i><Django class="iconosJx" /> </i>
+                    <p>Django</p>
+                </div>
+
+                {/* <div>
+                    <i className="fab fa-react fa-3x" style={{ color: '#61dafb' }}></i>
+                    <p>React</p>
+                </div> */}
+                <div>
+                    <i class="fa-brands fa-flutter fa-3x" style={{ color: '#02569B' }}></i>
+                    <p>Flutter</p>
+                </div>
+
         </Slider>
+        </div>
+
 
     );
 };
@@ -71,11 +87,14 @@ const MySlider2 = () => {
         arrows: false, 
         pauseOnHover: false,
         infinite: true,
-        speed: 1400,
-        slidesToShow: 6,
-        slidesToScroll: 1,
+        speed: 3200,
+        slidesToShow: 7,
+        slidesToScroll: 2,
         autoplay: true,
-        autoplaySpeed: 1500,
+        autoplaySpeed: 2500,
+        draggable: false, // Desactiva el arrastre
+        swipe: false, // Desactiva el deslizamiento en dispositivos táctiles
+        touchMove: false, // Desactiva el movimiento tácti
     };
 
     return (
@@ -83,22 +102,37 @@ const MySlider2 = () => {
 
             <Slider {...settings}>
                 <div>
-                    <i className="fab fa-html5 fa-3x" style={{ color: '#e34f26' }}></i>
+                    <i className="fa-brands fa-git-alt fa-3x" style={{ color: 'gray'}} ></i>
                 </div>
                 <div>
-                    <i className="fab fa-css3-alt fa-3x" style={{ color: '#1572b6' }}></i>
+                    <i class="fa-brands fa-github fa-3x" style={{ color: 'gray' }}></i>
                 </div>
                 <div>
-                    <i className="fab fa-js fa-3x" style={{ color: '#f7df1e' }}></i>
+                    <i class="fa-brands fa-windows fa-3x" style={{ color: 'gray' }}></i>
                 </div>
                 <div>
-                    <i className="fab fa-python fa-3x" style={{ color: '#306998' }}></i>
+                    <i class="fa-brands fa-linux fa-3x" style={{ color: 'gray' }}></i>
                 </div>
                 <div>
-                    <i className="fab fa-node-js fa-3x" style={{ color: '#339933' }}></i>
+                    <i><Kali className="size-12" /> </i>
                 </div>
                 <div>
-                    <i className="fab fa-react fa-3x" style={{ color: '#61dafb' }}></i>
+                    <i class="fa-brands fa-android fa-3x" style={{ color: 'gray' }}></i>
+                </div>
+                <div>
+                    <i><AndroidStudio /> </i>
+                </div>
+                <div>
+                    <i class="fa-brands fa-wordpress fa-3x" style={{ color: 'gray' }}></i>
+                </div>
+                <div>
+                    <i class="fa-brands fa-cpanel fa-3x" style={{ color: 'gray' }}></i>
+                </div>
+                <div>
+                    <i class="fa-solid fa-database fa-2x" style={{ color: 'gray' }}></i>
+                </div>
+                <div>
+                    <i class="fa-brands fa-figma fa-3x" style={{ color: 'gray' }}></i>
                 </div>
             </Slider>
         </div>
